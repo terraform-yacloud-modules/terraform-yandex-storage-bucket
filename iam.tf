9e4b7c320a04f8f456ddad523c6b6bad63fc1c86
Anton <patsev.anton@gmail.com>
2025-10-08 21:07:22 +0600

    use for_each

--- iam.tf
+++ iam.tf
@@ -1,21 +1,30 @@
 resource "yandex_iam_service_account" "storage_admin" {
-  count       = var.storage_admin_service_account.existing_account_id == null ? 1 : 0
+  for_each = var.storage_admin_service_account.existing_account_id == null ? {
+    create = true
+  } : {}
+
   name        = coalesce(var.storage_admin_service_account.name, local.storage_admin_service_account_name)
   description = var.storage_admin_service_account.description
   folder_id   = local.folder_id
 }
 
 resource "yandex_iam_service_account_static_access_key" "storage_admin" {
-  count              = var.storage_admin_service_account.existing_account_id == null ? 1 : 0
-  service_account_id = yandex_iam_service_account.storage_admin[0].id
+  for_each = var.storage_admin_service_account.existing_account_id == null ? {
+    create = true
+  } : {}
+
+  service_account_id = yandex_iam_service_account.storage_admin["create"].id
   description        = "Static access key for Object storage admin service account."
 }
 
 resource "yandex_resourcemanager_folder_iam_member" "storage_admin" {
-  count     = var.storage_admin_service_account.existing_account_id == null ? 1 : 0
+  for_each = var.storage_admin_service_account.existing_account_id == null ? {
+    create = true
+  } : {}
+
   folder_id = local.folder_id
   role      = "storage.admin"
-  member    = "serviceAccount:${yandex_iam_service_account.storage_admin[0].id}"
+  member    = "serviceAccount:${yandex_iam_service_account.storage_admin["create"].id}"
 
   depends_on = [yandex_iam_service_account_static_access_key.storage_admin]
 }
